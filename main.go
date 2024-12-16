320ad31b600c1927b16051b8a04b1ff123807510
Ben Hardy <107066058+benjaminhardy28@users.noreply.github.com>
2024-12-16 17:56:47 -0600

    integrated aws sdk with load balancer

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
+	"sync"
+	"time"
+	"github.com/aws/aws-sdk-go-v2/config"
+    "github.com/aws/aws-sdk-go-v2/service/autoscaling"
+    "github.com/aws/aws-sdk-go-v2/service/ec2"
 )
 
 type Server interface{
@@ -34,6 +41,7 @@ type LoadBalancer struct {
 	port	string
 	roundRobinCount 	int
 	servers 	[]Server
+	mutex           sync.RWMutex
 }
 
 
@@ -76,20 +84,106 @@ func (lb *LoadBalancer) serveProxy(rw http.ResponseWriter, req *http.Request){
 	targetServer.Serve(rw, req)
 }
 
-func main() {
-	servers := []Server{
-		newSimpleServer("http://localhost:8081/"),
-		newSimpleServer("http://localhost:8082/"),
+func (lb *LoadBalancer) updateServers(newServers []Server) {
+	lb.mutex.Lock()
+	defer lb.mutex.Unlock()
+	lb.servers = newServers
+ }
+ 
+
+func fetchEC2Instances(asgClient *autoscaling.Client, ec2Client *ec2.Client, groupName string) ([]Server, error) {
+	output, err := asgClient.DescribeAutoScalingGroups(context.TODO(), &autoscaling.DescribeAutoScalingGroupsInput{ //gets list of instances managed by ASG
+		AutoScalingGroupNames: []string{groupName},
+	})
+	if err != nil {
+		return nil, err
 	}
+ 
+ 
+	asg := output.AutoScalingGroups[0]
+ 
+ 
+	if len(asg.Instances) == 0 {
+		log.Printf("ASG %s exists but has no managed instances", groupName)
+		return []Server{}, nil
+	}
+ 
+ 
+	if len(output.AutoScalingGroups) == 0 {
+		return nil, fmt.Errorf("no Auto Scaling Group found with name %s", groupName)
+	}
+ 
+ 
+	var instanceIDs []string
+	for _, instance := range output.AutoScalingGroups[0].Instances {
+		if instance.LifecycleState == "InService" {
+			instanceIDs = append(instanceIDs, *instance.InstanceId)
+		}
+	}
+ 
+ 
+	ec2Output, err := ec2Client.DescribeInstances(context.TODO(), &ec2.DescribeInstancesInput{ //retreives more information, like the IP address of each instance
+		InstanceIds: instanceIDs,
+	})
+	if err != nil {
+		return nil, err
+	}
+ 
+ 
+	var servers []Server
+	for _, reservation := range ec2Output.Reservations { //reservations represent a grouping of EC2 instances returned
+		for _, instance := range reservation.Instances { //goes through each instance in reservation
+			if instance.PublicIpAddress != nil {
+				address := fmt.Sprintf("http://%s:8081", *instance.PublicIpAddress) // ec2 intance accepts requests to port 8081 for api
+				servers = append(servers, newSimpleServer(address))
+			}
+		}
+	}
+ 
+ 
+	return servers, nil
+ }
+ 
 
-	lb := newLoadBalancer("8000", servers)
-
-	handleRedirect := func(rw http.ResponseWriter, req *http.Request){
+func main() {
+	// AWS SDK Configuration
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-east-2"))
+	handleErr(err)
+ 
+ 
+	asgClient := autoscaling.NewFromConfig(cfg)
+	ec2Client := ec2.NewFromConfig(cfg)
+ 
+ 
+	// Auto Scaling Group Name
+	autoScalingGroupName := "lbProjectASGTemplate"
+ 
+ 
+	lb := newLoadBalancer("8000", []Server{})
+ 
+ 
+	go func() {
+		for {
+			newServers, err := fetchEC2Instances(asgClient, ec2Client, autoScalingGroupName)
+			if err != nil {
+				log.Printf("Error updating servers: %v", err)
+			} else {
+				lb.updateServers(newServers)
+				log.Printf("Updated server list: %v", newServers)
+			}
+			time.Sleep(1 * time.Minute) // Update every 5 minutes
+		}
+	}()
+ 
+ 
+	handleRedirect := func(rw http.ResponseWriter, req *http.Request) {
 		lb.serveProxy(rw, req)
 	}
-
+ 
+ 
 	http.HandleFunc("/", handleRedirect)
-
-	fmt.Printf("serving requests at 'localhost:%s'\n", lb.port)
+ 
+ 
+	fmt.Printf("Serving requests at 'localhost:%s'\n", lb.port)
 	http.ListenAndServe(":"+lb.port, nil)
 }
